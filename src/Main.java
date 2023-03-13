401b2121909327f09e47368962aa782e70ccc05d
Emma Paris <eparis@atlassian.com>
2023-03-13 09:16:28 +0300

    src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // Объявляете переменные для входных данных и
+        // параметров программы: одну для хранения
+        // стоимости билета, другую для хранения количества
+        // рублей для одной бонусной мили
+
+        // Рассчитываете количество бонусных миль, используя
+        // значения заведённых переменных. Ответ сохраняете в
+        // новую переменную и выводите на экран
+        int ticketPrice = 10000;
+        int variableForCalculatingTheBonus = 20;
+
+        System.out.println("За полёт Вам начислены мили:");
+        System.out.println((ticketPrice / variableForCalculatingTheBonus) + " миль");
     }
 }
